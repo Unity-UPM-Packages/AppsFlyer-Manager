f59ecccc1bfa43a76596a95e7a3a3ea1e57fb62e
QuangHaiVu <vuquanghai0108@gmail.com>
2025-11-17 10:58:57 +0700

    fix: add conversionDataStatus

---
+++ Runtime/Scripts/GetConversionDataStatus.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+namespace TheLegends.Base.AppsFlyer
+{
+    public enum GetConversionDataStatus
+    {
+        None = 0,
+        Fetching = 1,
+        Success = 2,
+        Fail = 3,
+    }
+}
